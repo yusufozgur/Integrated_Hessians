eff6ec1dc239a5aa178f688d457e7bb603839563
Yusuf Turhan <yusufozgurtr@gmail.com>
2026-03-12 08:56:56 +0100

    add hessian function alongside relu to softplus replacement monkey patching

--- src/integrated_hessians/simulation/infer_notebook.py
+++ src/integrated_hessians/simulation/infer_notebook.py
@@ -4,6 +4,60 @@ __generated_with = "0.20.4"
 app = marimo.App(width="medium")
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Plot
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    row = mo.ui.number(0,1000,1, label="Choose Row")
+    row
+    return (row,)
+
+
+@app.cell
+def _(
+    attributions_permuted,
+    onehot_permuted,
+    plot_binary_string,
+    plot_heatmap,
+    plot_onehot,
+    plt,
+    pred,
+    real_attributions,
+    seq,
+):
+    fig, axes = plt.subplots(
+        nrows=5,
+        figsize=(12, 6),
+        sharex=True,                          # guarantees column alignment
+        gridspec_kw={'height_ratios': [4, 1, 1, 4, 1]} # optional: scale row heights by number of rows
+    )
+
+    plot_onehot(seq.nucleotides, onehot_permuted, axes[0], title=f"Phen: {seq.phenotype} Pred: {pred: .2}")
+    plot_binary_string(seq.motif_mask_1, axes[1], title=seq.motif_types[0])
+    plot_binary_string(seq.motif_mask_2, axes[2], title=seq.motif_types[1])
+    plot_onehot(seq.nucleotides, attributions_permuted, axes[3], title="Integrated Gradients (Multiplied by Input)")
+    plot_heatmap(real_attributions, row_labels=["Real base"], col_labels=list(seq.nucleotides), ax=axes[4], cmap='Blues', title="Real Attributions")
+
+    axes[0].set_xlabel("")  # remove redundant x-label from top plot
+    plt.tight_layout()
+    plt.gca()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Setup
+    """)
+    return
+
+
 @app.cell
 def _():
     import marimo as mo
@@ -55,6 +109,7 @@ def _(CNNDense, torch):
 
 @app.cell
 def _(model, seq, torch):
+    # Calculate Model Prediction
     with torch.no_grad():
         pred = model(torch.tensor(seq.one_hot).unsqueeze(0).type(torch.float))
     pred = float(pred[0,0])
@@ -62,7 +117,19 @@ def _(model, seq, torch):
 
 
 @app.cell
+def _(IntegratedGradients, model, seq, torch):
+    #Calculate Attributions
+    input = torch.tensor(seq.one_hot).unsqueeze(0).type(torch.float)
+    baseline = torch.full_like(input, 0.25)
+    ig = IntegratedGradients(model, multiply_by_inputs=True)
+    attributions, delta = ig.attribute(input, baseline, return_convergence_delta=True)
+    f"Delta: {float(delta): .2}"
+    return attributions, baseline
+
+
+@app.cell
 def _(attributions, np, seq):
+    # Get attributions for existent positions (One hot -> Real)
     onehot_permuted = np.permute_dims(seq.one_hot, [1,0])
     attributions_permuted = np.permute_dims(attributions.squeeze(0).numpy(),[1,0])
     real_attributions = np.sum(onehot_permuted * attributions_permuted, axis=0).reshape(1, -1)
@@ -70,77 +137,32 @@ def _(attributions, np, seq):
 
 
 @app.cell
-def _(mo):
-    row = mo.ui.number(0,1000,1)
-    row
-    return (row,)
+def _(baseline, model):
+    # Calculate Hessian
+    from integrated_hessians.hessian import hessian
+    hess = hessian(model, baseline, target=0)
+    hess.shape
+    return (hess,)
 
 
 @app.cell
-def _(
-    attributions_permuted,
-    onehot_permuted,
-    plot_binary_string,
-    plot_heatmap,
-    plot_onehot,
-    plt,
-    pred,
-    real_attributions,
-    seq,
-):
-    fig, axes = plt.subplots(
-        nrows=5,
-        figsize=(12, 6),
-        sharex=True,                          # guarantees column alignment
-        gridspec_kw={'height_ratios': [4, 1, 1, 4, 1]} # optional: scale row heights by number of rows
-    )
-
-    plot_onehot(seq.nucleotides, onehot_permuted, axes[0], title=f"Phen: {seq.phenotype} Pred: {pred: .2}")
-    plot_binary_string(seq.motif_mask_1, axes[1], title=seq.motif_types[0])
-    plot_binary_string(seq.motif_mask_2, axes[2], title=seq.motif_types[1])
-    plot_onehot(seq.nucleotides, attributions_permuted, axes[3], title="Integrated Gradients (Multiplied by Input)")
-    plot_heatmap(real_attributions, row_labels=["Real base"], col_labels=list(seq.nucleotides), ax=axes[4], cmap='Blues', title="Real Attributions")
-
-    axes[0].set_xlabel("")  # remove redundant x-label from top plot
-    plt.tight_layout()
-    plt.gca()
+def _(hess, torch):
+    torch.max(hess.reshape(200, 200))
     return
 
 
 @app.cell
-def _(seq, torch):
-    input = torch.tensor(seq.one_hot).unsqueeze(0).type(torch.float)
-    baseline = torch.full_like(input, 0.25)
-    return baseline, input
-
-
-@app.cell
-def _(IntegratedGradients, baseline, input, model):
-    ig = IntegratedGradients(model, multiply_by_inputs=True)
-    attributions, delta = ig.attribute(input, baseline, return_convergence_delta=True)
-    attributions, delta
-    return (attributions,)
-
-
-@app.cell
-def _(attributions):
-    attributions[0,:,:].shape
-    return
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    Search sample
-    """)
+def _(hess, plt):
+    plt.imshow(hess.reshape(200, 200))
     return
 
 
 @app.cell
 def _(MotifType, mo):
+    # Search Dataset
     search1 = mo.ui.dropdown(options=list(MotifType))
     search2 = mo.ui.dropdown(options=list(MotifType))
-    search1, search2
+    "Search in dataset", search1, search2
     return search1, search2
 
 
@@ -149,7 +171,8 @@ def _(search1, search2, seqs):
     for i,x in enumerate(seqs):
         if x.motif_types[0] == search1.value:
             if x.motif_types[1] == search2.value:
-                print(i)
+                print(f"Found specified datapoint at {i}")
+                break
     return
 
 
