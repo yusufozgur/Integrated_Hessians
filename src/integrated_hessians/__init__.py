integrated hessian implementation and api with middle riemann fix and self interaction term fix is done

src/integrated_hessians/__init__.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from typing import Callable, Annotated, Literal, Tuple
+from typing import Callable, Annotated, Literal, Tuple, Union
 from beartype import beartype
 from torch import Tensor
 import torch
@@ -71,7 +71,7 @@ def get_hessian(
     with replace_relu_with_softplus():
         hessian_result: jx.Float[
             Tensor, "batch_size *input_shape batch_size *input_shape"
-        ] = torch.autograd.functional.hessian(forward_func, input, strict=True)
+        ] = torch.autograd.functional.hessian(forward_func, input, strict=True)  # type: ignore
 
     # jaxtyping does not support multiple variadic specifiers (*name,). So, instead, I am checking the output is correct shape via an assert statement
     assert hessian_result.shape == (*input.shape, *input.shape)
@@ -135,7 +135,7 @@ def _get_integrated_hessian_for_single_sample(
 
 
 @jx.jaxtyped(typechecker=beartype)
-def get_integrated_hessians(
+def _deleteme_get_integrated_hessians(
     model: Callable,
     inputs: jx.Float[Tensor, " batch_size *input_shape"],
     baselines: jx.Float[Tensor, " batch_size *input_shape"],
@@ -184,3 +184,253 @@ def get_integrated_hessians(
         return global_interactions, deltas
     else:
         return local_interactions, deltas
+
+
+@jx.jaxtyped(typechecker=beartype)
+def _get_common_term(
+    func: Callable[
+        [jx.Float[Tensor, " input_shape_flattened "]],
+        jx.Float[Tensor, ""],
+    ],
+    inputs_flattened: jx.Float[Tensor, "batch_size input_shape_flattened"],
+    baselines_flattened: jx.Float[Tensor, "batch_size input_shape_flattened "],
+    approximation_steps: int,
+) -> jx.Float[Tensor, "batch_size input_shape_flattened input_shape_flattened"]:
+    """
+    If i != j, then
+
+    IH = (xi - xi') (xj - xj') sum_l=1^k (sum_p=1^m( l/k*p/m * df(x' + (l/k) (x - x'))/(dxi dxj) 1/k/m ))
+
+    We name term the common term, as it is calculated for both i==j and i!=j cases, the extra term for the i==j case is calculated elsewhere
+    """
+    batch_shape = inputs_flattened.shape[0]
+    flattened_input_shape = inputs_flattened.shape[1]
+
+    # this carries the riemann sum
+    second_order_sensitivity = torch.zeros(
+        batch_shape, flattened_input_shape, flattened_input_shape
+    )
+    k = approximation_steps
+    m = approximation_steps
+
+    # Important: we use the middle riemann sum, because it approximates better than right riemann sum. Usage of (l - .5) and (p - .5) is due to the middle riemann sum.
+    for l in range(1, k + 1):  # noqa: E741
+        beta: float = (l - 0.5) / k  # -.5 is for getting the middle riemann sum
+        for p in range(1, m + 1):
+            alpha: float = (p - 0.5) / m
+
+            # alphabeta is the combined interpolation coefficient
+            alphabeta: float = beta * alpha
+            # This is one point on the path integral
+            interpolation: jx.Float[Tensor, "batch_size input_shape_flattened "] = (
+                baselines_flattened
+                + alphabeta * (inputs_flattened - baselines_flattened)
+            )
+
+            get_second_order_grad = torch.vmap(torch.func.hessian(func=func))
+            second_order_grad: jx.Float[
+                Tensor, "batch_size input_shape_flattened input_shape_flattened"
+            ] = get_second_order_grad(interpolation)
+
+            second_order_sensitivity += second_order_grad * alphabeta * 1 / k / m
+
+    diff = inputs_flattened - baselines_flattened
+    outer_product = diff.unsqueeze(dim=1) * diff.unsqueeze(dim=2)
+    common_term = second_order_sensitivity * outer_product
+    return common_term
+
+
+@jx.jaxtyped(typechecker=beartype)
+def _get_self_interaction_extra_term(
+    func: Callable[
+        [jx.Float[Tensor, " input_shape_flattened "]],
+        jx.Float[Tensor, ""],
+    ],
+    inputs_flattened: jx.Float[Tensor, "batch_size input_shape_flattened"],
+    baselines_flattened: jx.Float[Tensor, "batch_size input_shape_flattened "],
+    approximation_steps: int,
+) -> jx.Float[Tensor, "batch_size input_shape_flattened"]:
+    """
+    if i == j, then
+
+    IH = (xi - xi') sum_l=1^k(sum_p=1^m( df(x' + (l/k) (x - x'))/dxi 1/k/m )) + (xi - xi') (xj - xj') sum_l=1^k(sum_p=1^m( l/k*p/m * df(x' + (l/k) (x - x'))/(dxi dxj) 1/k/m ))
+
+    The second term is already calculated at _get_common_term(), here, we calculate  the first term.
+    """
+    batch_shape = inputs_flattened.shape[0]
+    flattened_input_shape = inputs_flattened.shape[1]
+
+    # this carries the riemann sum
+    self_interaction_term = torch.zeros(batch_shape, flattened_input_shape)
+    k = approximation_steps
+    m = approximation_steps
+
+    # Important: we use the middle riemann sum, because it approximates better than right riemann sum. Usage of (l - .5) and (p - .5) is due to the middle riemann sum.
+    for l in range(1, k + 1):  # noqa: E741
+        beta: float = (l - 0.5) / k  # -.5 is for getting the middle riemann sum
+        for p in range(1, m + 1):
+            alpha: float = (p - 0.5) / m
+
+            # alphabeta is the combined interpolation coefficient
+            alphabeta: float = beta * alpha
+            # This is one point on the path integral
+            interpolation: jx.Float[Tensor, "batch_size input_shape_flattened "] = (
+                baselines_flattened
+                + alphabeta * (inputs_flattened - baselines_flattened)
+            )
+
+            first_order_grad: jx.Float[
+                Tensor, "batch_size input_shape_flattened input_shape_flattened"
+            ] = torch.vmap(torch.func.jacrev(func=func))(interpolation)
+
+            self_interaction_term += first_order_grad * 1 / k / m
+
+    diff = inputs_flattened - baselines_flattened
+    self_interaction_term = diff * self_interaction_term
+    return self_interaction_term
+
+
+@jx.jaxtyped(typechecker=beartype)
+def _get_delta(
+    func: Callable[
+        [jx.Float[Tensor, " batch_size input_shape_flattened "]],
+        jx.Float[Tensor, " batch_size "],
+    ],
+    inputs_flattened: jx.Float[Tensor, "batch_size input_shape_flattened"],
+    baselines_flattened: jx.Float[Tensor, "batch_size input_shape_flattened "],
+    interaction_attributions: jx.Float[
+        Tensor, "batch_size input_shape_flattened input_shape_flattened"
+    ],
+) -> jx.Float[Tensor, " batch_size "]:
+    f_input: jx.Float[Tensor, " batch_size 1 "] = func(inputs_flattened)
+    f_baseline: jx.Float[Tensor, " batch_size 1 "] = func(inputs_flattened)
+
+    out_diff: jx.Float[Tensor, " batch_size 1 "] = f_input - f_baseline
+
+    interaction_attr_sum: jx.Float[Tensor, " batch_size "] = (
+        interaction_attributions.sum(dim=-1).sum(dim=-1)
+    )
+
+    delta: jx.Float[Tensor, " batch_size "] = out_diff - interaction_attr_sum
+
+    return delta
+
+
+def get_integrated_hessians(
+    model: Callable[
+        [jx.Float[Tensor, " batch_size *input_shape "]],
+        jx.Float[Tensor, " batch_size *output_shape "],
+    ],
+    inputs: jx.Float[Tensor, " batch_size *input_shape "],
+    baselines: jx.Float[Tensor, " batch_size *input_shape "],
+    target: Union[int, Tuple[int, ...]],
+    approximation_steps=50,
+) -> Tuple[
+    Annotated[Tensor, "batch_size *input_shape"],  # interaction attributions
+    Annotated[Tensor, "batch_size"],  # deltas
+]:
+    assert len(inputs.shape) >= 2, (
+        "inputs must have at least two dimensions, one bath dimension and one or more input dimensions"
+    )
+
+    batch_shape = inputs.shape[0]
+    input_shape = inputs.shape[1:]
+
+    assert baselines.shape == (batch_shape, *input_shape), (
+        "Input tensor and baseline tensor must have the same shape"
+    )
+
+    # instead of an unknown shape, it is easier to operate on the flattened tensors
+
+    inputs_flattened: jx.Float[Tensor, "batch_size input_shape_flattened"] = (
+        inputs.reshape(batch_shape, -1)
+    )
+    baselines_flattened: jx.Float[Tensor, "batch_size input_shape_flattened "] = (
+        baselines.reshape(batch_shape, -1)
+    )
+
+    input_shape_flattened = inputs_flattened.shape[1]
+
+    @jx.jaxtyped(typechecker=beartype)
+    def func_with_scalar_output(
+        x: jx.Float[Tensor, "batch_size input_shape_flattened"],
+    ) -> jx.Float[Tensor, " batch_size "]:
+        """
+
+        - output a scalar
+        - handle flat inputs
+        """
+        assert len(x.shape) == 2
+        assert x.shape[1] == input_shape_flattened
+        x_batch_size = x.shape[0]
+        # convert to original shape so the user supplied function can accept it
+        flat_to_original = x.reshape(x_batch_size, *input_shape)
+        output = model(flat_to_original)
+        # target does subsetting to get the scalar outputs, so it could be an int or tuple of ints
+        scalar_output = output[:, target]
+        assert scalar_output.shape == (
+            x_batch_size,
+        ), (  # the 1 comes from the unsqueeze
+            "Could not reduce output dimension to scalars, the target given for subsetting may be incorrect."
+        )
+        return scalar_output
+
+    @jx.jaxtyped(typechecker=beartype)
+    def func_with_scalar_output_and_transient_batch_dimentsion(
+        x: jx.Float[Tensor, "input_shape_flattened"],
+    ) -> jx.Float[Tensor, ""]:
+        """
+        Two goals
+        - squeeze and unsqueeze to satisfy torch.func.vmap
+        - output a scalar
+        - handle flat inputs
+        """
+        # as we use torch.func.vmap, the batch dimension will come here stripped
+        assert x.shape == (input_shape_flattened,)
+        # add a transient batch dimension
+        x_transient_batch = x.unsqueeze(0)
+
+        scalar_output = func_with_scalar_output(x_transient_batch)
+
+        # remove the transient batch dimension
+        scalar_output_no_batch_dim = scalar_output.squeeze(0)
+        assert scalar_output_no_batch_dim.shape == ()
+        return scalar_output_no_batch_dim
+
+    with replace_relu_with_softplus():
+        # used in both i!=j and i==j cases.
+        common_term: jx.Float[
+            Tensor, "batch_size input_shape_flattened input_shape_flattened"
+        ] = _get_common_term(
+            func=func_with_scalar_output_and_transient_batch_dimentsion,
+            inputs_flattened=inputs_flattened,
+            baselines_flattened=baselines_flattened,
+            approximation_steps=approximation_steps,
+        )
+        # self_interaction_extra_terms are calculated for diagonals(i==j), it exists due to the chain rule of calculus when you take d(IG(xi))/dxi.
+        self_interaction_extra_term: jx.Float[
+            Tensor, "batch_size input_shape_flattened"
+        ] = _get_self_interaction_extra_term(
+            func=func_with_scalar_output_and_transient_batch_dimentsion,
+            inputs_flattened=inputs_flattened,
+            baselines_flattened=baselines_flattened,
+            approximation_steps=approximation_steps,
+        )
+
+        # this operation add self interaction term only onto the diagonals of the common term
+        interaction_attributions: jx.Float[
+            Tensor, "batch_size input_shape_flattened input_shape_flattened"
+        ] = common_term + self_interaction_extra_term.diag_embed()
+
+        delta = _get_delta(
+            func=func_with_scalar_output,
+            inputs_flattened=inputs,
+            baselines_flattened=baselines,
+            interaction_attributions=interaction_attributions,
+        )
+
+    interaction_attributions_original_shape = interaction_attributions.reshape(
+        batch_shape, *input_shape, *input_shape
+    )
+
+    return interaction_attributions_original_shape, delta

src/integrated_hessians/simulation/test_model_notebook.py
@@ -160,7 +160,7 @@ def _(
     interpolate_onehot,
     jx,
     model,
-    one_hot: "jx.Float[NDArray[np.float32], \"alphabet_length sequence_length\"]",
+    one_hot: 'jx.Float[NDArray[np.float32], "alphabet_length sequence_length"]',
     plot_epistasis_subsetted,
     subset_onehot_hessian,
     torch,
@@ -198,9 +198,9 @@ def _():
 
 @app.cell
 def _():
-    from integrated_hessians import get_integrated_hessians
+    from integrated_hessians import _deleteme_get_integrated_hessians
 
-    return (get_integrated_hessians,)
+    return (_deleteme_get_integrated_hessians,)
 
 
 @app.cell
@@ -234,7 +234,7 @@ def _(integ_hess_result):
 def _(
     ih_delta,
     integ_hess_result,
-    one_hot: "jx.Float[NDArray[np.float32], \"alphabet_length sequence_length\"]",
+    one_hot: 'jx.Float[NDArray[np.float32], "alphabet_length sequence_length"]',
     plot_epistasis_subsetted,
     subset_onehot_hessian,
     torch,
@@ -292,7 +292,9 @@ def _(one_hot_batched, torch):
 
 @app.cell
 def _(exp, exp_baseline, exp_input):
-    exp_ih = exp.interactions(exp_input, exp_baseline, num_samples=1000, use_expectation=False)
+    exp_ih = exp.interactions(
+        exp_input, exp_baseline, num_samples=1000, use_expectation=False
+    )
     return (exp_ih,)
 
 
@@ -304,7 +306,7 @@ def _(exp_ih):
 
 @app.cell
 def _(exp_ih):
-    exp_ih_reshaped = exp_ih.reshape(1,50,4,50,4)
+    exp_ih_reshaped = exp_ih.reshape(1, 50, 4, 50, 4)
     exp_ih_reshaped.shape
     return (exp_ih_reshaped,)
 
@@ -330,7 +332,7 @@ def _(exp_ih_reshaped):
 @app.cell
 def _(
     exp_ih_reshaped,
-    one_hot: "jx.Float[NDArray[np.float32], \"alphabet_length sequence_length\"]",
+    one_hot: 'jx.Float[NDArray[np.float32], "alphabet_length sequence_length"]',
     plot_epistasis_subsetted,
     subset_onehot_hessian,
     torch,