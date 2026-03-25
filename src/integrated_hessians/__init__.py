fix bugs: detach for memory overflow and fix type in delta calculation

src/integrated_hessians/__init__.py
@@ -214,7 +214,7 @@ def _get_common_term(
     m = approximation_steps
 
     # Important: we use the middle riemann sum, because it approximates better than right riemann sum. Usage of (l - .5) and (p - .5) is due to the middle riemann sum.
-    for l in range(1, k + 1):  # noqa: E741
+    for l in tqdm(range(1, k + 1)):  # noqa: E741
         beta: float = (l - 0.5) / k  # -.5 is for getting the middle riemann sum
         for p in range(1, m + 1):
             alpha: float = (p - 0.5) / m
@@ -230,7 +230,7 @@ def _get_common_term(
             get_second_order_grad = torch.vmap(torch.func.hessian(func=func))
             second_order_grad: jx.Float[
                 Tensor, "batch_size input_shape_flattened input_shape_flattened"
-            ] = get_second_order_grad(interpolation)
+            ] = get_second_order_grad(interpolation).detach()
 
             second_order_sensitivity += second_order_grad * alphabeta * 1 / k / m
 
@@ -266,7 +266,7 @@ def _get_self_interaction_extra_term(
     m = approximation_steps
 
     # Important: we use the middle riemann sum, because it approximates better than right riemann sum. Usage of (l - .5) and (p - .5) is due to the middle riemann sum.
-    for l in range(1, k + 1):  # noqa: E741
+    for l in tqdm(range(1, k + 1)):  # noqa: E741
         beta: float = (l - 0.5) / k  # -.5 is for getting the middle riemann sum
         for p in range(1, m + 1):
             alpha: float = (p - 0.5) / m
@@ -281,7 +281,7 @@ def _get_self_interaction_extra_term(
 
             first_order_grad: jx.Float[
                 Tensor, "batch_size input_shape_flattened input_shape_flattened"
-            ] = torch.vmap(torch.func.jacrev(func=func))(interpolation)
+            ] = torch.vmap(torch.func.jacrev(func=func))(interpolation).detach()
 
             self_interaction_term += first_order_grad * 1 / k / m
 
@@ -303,7 +303,7 @@ def _get_delta(
     ],
 ) -> jx.Float[Tensor, " batch_size "]:
     f_input: jx.Float[Tensor, " batch_size 1 "] = func(inputs_flattened)
-    f_baseline: jx.Float[Tensor, " batch_size 1 "] = func(inputs_flattened)
+    f_baseline: jx.Float[Tensor, " batch_size 1 "] = func(baselines_flattened)
 
     out_diff: jx.Float[Tensor, " batch_size 1 "] = f_input - f_baseline
 

src/integrated_hessians/simulation/test_model_notebook.py
@@ -30,7 +30,7 @@ def _():
         plot_binary_string,
         plot_heatmap,
     )
-    from integrated_hessians import get_hessian
+    from integrated_hessians import get_hessian, get_integrated_hessians
     from integrated_hessians.simulation.test_model import (
         get_test_data,
         get_model,
@@ -47,6 +47,7 @@ def _():
         SimulatedSequence,
         get_attributions,
         get_hessian,
+        get_integrated_hessians,
         get_model,
         get_prediction,
         get_test_data,
@@ -160,7 +161,7 @@ def _(
     interpolate_onehot,
     jx,
     model,
-    one_hot: 'jx.Float[NDArray[np.float32], "alphabet_length sequence_length"]',
+    one_hot: "jx.Float[NDArray[np.float32], \"alphabet_length sequence_length\"]",
     plot_epistasis_subsetted,
     subset_onehot_hessian,
     torch,
@@ -200,25 +201,30 @@ def _():
 def _():
     from integrated_hessians import _deleteme_get_integrated_hessians
 
-    return (_deleteme_get_integrated_hessians,)
+    return
 
 
 @app.cell
 def _(mo):
-    sampling_steps = mo.ui.number(0, 100, 1, label="Sampling steps", value=1)
+    sampling_steps = mo.ui.number(0, 100, 1, label="Sampling steps", value=50)
     sampling_steps
     return (sampling_steps,)
 
 
 @app.cell
+def _(model, one_hot_batched, torch):
+    model(one_hot_batched) - model(torch.full_like(one_hot_batched, 0.25))
+    return
+
+
+@app.cell
 def _(get_integrated_hessians, model, one_hot_batched, sampling_steps, torch):
     integ_hess_result, ih_delta = get_integrated_hessians(
-        model,
-        one_hot_batched,
-        torch.full_like(one_hot_batched, 0.25),
-        0,
-        integration_steps=sampling_steps.value,
-        multiply_by_inputs=True,
+        model=model,
+        inputs=one_hot_batched,
+        baselines=torch.full_like(one_hot_batched, 0.25),
+        target=0,
+        approximation_steps=sampling_steps.value,
     )
     integ_hess_result.shape
     return ih_delta, integ_hess_result
@@ -234,7 +240,7 @@ def _(integ_hess_result):
 def _(
     ih_delta,
     integ_hess_result,
-    one_hot: 'jx.Float[NDArray[np.float32], "alphabet_length sequence_length"]',
+    one_hot: "jx.Float[NDArray[np.float32], \"alphabet_length sequence_length\"]",
     plot_epistasis_subsetted,
     subset_onehot_hessian,
     torch,
@@ -243,7 +249,9 @@ def _(
         hessian_onehot_subsetted=subset_onehot_hessian(
             calculated_hessian=integ_hess_result.squeeze(0),
             one_hot_mask=torch.tensor(one_hot),
-        ).numpy(),
+        )
+        .detach()
+        .numpy(),
         title=f"Integrated hessians. delta: {ih_delta[0]: .3f}",
     )
     return
@@ -251,7 +259,7 @@ def _(
 
 @app.cell
 def _(integ_hess_result, plt):
-    plt.imshow(integ_hess_result.reshape(200, 200), cmap="bwr")
+    plt.imshow(integ_hess_result.detach().reshape(200, 200), cmap="bwr")
     return
 
 
@@ -293,7 +301,7 @@ def _(one_hot_batched, torch):
 @app.cell
 def _(exp, exp_baseline, exp_input):
     exp_ih = exp.interactions(
-        exp_input, exp_baseline, num_samples=1000, use_expectation=False
+        exp_input, exp_baseline, num_samples=10, use_expectation=False
     )
     return (exp_ih,)
 
@@ -332,7 +340,7 @@ def _(exp_ih_reshaped):
 @app.cell
 def _(
     exp_ih_reshaped,
-    one_hot: 'jx.Float[NDArray[np.float32], "alphabet_length sequence_length"]',
+    one_hot: "jx.Float[NDArray[np.float32], \"alphabet_length sequence_length\"]",
     plot_epistasis_subsetted,
     subset_onehot_hessian,
     torch,