de1a1bdd8bc408ad187036125a49bba6a665cf2c
Yusuf Turhan <yusufozgurtr@gmail.com>
2026-05-01 09:44:35 +0200

    batch the IH testing for faster processing

--- src/integrated_hessians/simulation/test_method/implementation_performance_comparison.py
+++ src/integrated_hessians/simulation/test_method/implementation_performance_comparison.py
@@ -17,11 +17,13 @@ Dataset: Custom sim with expanded distribution
 
 import json
 import functools
+from os import cpu_count
 from pathlib import Path
 from typing import Callable
 import jaxtyping as jx
 import torch
 from torch import Tensor
+from torch.utils.data import DataLoader, Subset
 from tqdm import tqdm
 from integrated_hessians import get_integrated_hessians
 from integrated_hessians.simulation import SimulatedSequence
@@ -32,12 +34,15 @@ from integrated_hessians.simulation.test_method.path_explain_wrapper import (
 from integrated_hessians.simulation.test_model import get_test_data
 from dataclasses import dataclass, asdict
 
+from integrated_hessians.simulation.train_model import MotifInteractionsDataset
+
 # Configuration
 CONFIGPATH = (
     "src/integrated_hessians/simulation/configs/custom_expanded_distribution.json"
 )
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-NUM_OF_ROWS = 5
+BATCH_SIZE = 100
+NUM_OF_ROWS = 100
 BASELINE_FILL = 0.25
 SAVE_test_deltas_per_impl = (
     "src/integrated_hessians/simulation/test_method/test_deltas_per_impl.json"
@@ -104,7 +109,10 @@ def main():
         impl_name: [
             asdict(x)
             for x in get_delta_per_test_row(
-                test_data=test_data, implementation_config=impl_config, model=model
+                test_data=test_data,
+                implementation_config=impl_config,
+                model=model,
+                config=config,
             )
         ]
         for impl_name, impl_config in implementations.items()
@@ -115,7 +123,10 @@ def main():
 
 
 def get_delta_per_test_row(
-    implementation_config: dict, test_data: list[SimulatedSequence], model: Callable
+    implementation_config: dict,
+    test_data: list[SimulatedSequence],
+    model: Callable,
+    config: dict,
 ) -> list[PerformanceTest]:
     num_of_function_calls = 0
 
@@ -124,25 +135,39 @@ def get_delta_per_test_row(
         num_of_function_calls += 1
         return model(*args, **kwargs)
 
-    results = []
-    for test_data_idx in tqdm(range(NUM_OF_ROWS)):
-        test_row: SimulatedSequence = test_data[test_data_idx]
-        input: jx.Float[Tensor, "1 alphabet_length sequence_length"] = (
-            torch.from_numpy(test_row.one_hot).unsqueeze(0).type(torch.float).to(DEVICE)
-        )
-        baseline = torch.full_like(input, fill_value=BASELINE_FILL).to(DEVICE)
+    dataset = MotifInteractionsDataset(
+        input=Path(config["TEST_DATA"]),
+        SEQLEN=config["SEQLEN"],
+        EXPAND_DATA_DISTRIBUTION_ALONG_BASELINE_TO_INPUT_PATH=False,
+    )
 
+    dataset = Subset(dataset, range(NUM_OF_ROWS))
+
+    num_of_cores = cpu_count()
+    if num_of_cores is None:
+        num_of_cores = 1
+    dataloader = DataLoader(
+        dataset=dataset, batch_size=BATCH_SIZE, num_workers=num_of_cores
+    )
+    results = []
+    for inputs, labels in tqdm(dataloader):
+        inputs = inputs.to(DEVICE).to(torch.float)
+        baselines = torch.full_like(inputs, fill_value=BASELINE_FILL).to(DEVICE)
+
+        ih_delta: Tensor
         integ_hess_result, ih_delta = implementation_config["f"](
             approximation_steps=implementation_config["approx_steps"],
-            inputs=input,
-            baselines=baseline,
+            inputs=inputs,
+            baselines=baselines,
             model=model_call_counter,
         )
-        ih_delta = float(ih_delta[0])  # [0] to remove the batch dim
+        ih_deltas = ih_delta.detach().tolist()  # [0] to remove the batch dim
 
-        results.append(
-            PerformanceTest(delta=ih_delta, function_calls=num_of_function_calls)
-        )
+        results += [
+            PerformanceTest(delta=x, function_calls=num_of_function_calls)
+            for x in ih_deltas
+        ]
+        num_of_function_calls = 0
     return results
 
 
--- src/integrated_hessians/simulation/test_method/path_explain_wrapper.py
+++ src/integrated_hessians/simulation/test_method/path_explain_wrapper.py
@@ -13,24 +13,27 @@ def path_explain_wrapper(
     approximation_steps: int,
     baseline_fill=0.25,
 ):
+    batch_size = inputs.shape[0]
     seqlen = inputs.shape[1]
 
     def exp_reshaper(x: Tensor):
-        x = x.reshape((1, seqlen, 4))
+        x = x.reshape((batch_size, seqlen, 4))
         x = model(x)
         return x
 
     exp = PathExplainerTorch(exp_reshaper)
 
-    exp_input = inputs.reshape(1, seqlen * 4)
-    exp_baseline = baselines.reshape(1, seqlen * 4)
+    exp_input = inputs.reshape(batch_size, seqlen * 4)
+    exp_baseline = baselines.reshape(batch_size, seqlen * 4)
 
     exp_input.requires_grad_(True)
     exp_baseline.requires_grad_(True)
 
     exp_ih = exp.interactions(
         exp_input,
-        exp_baseline,
+        exp_baseline[
+            0
+        ],  # unlike our api, where we can take one baseline corresponding to one input, path_explain accepts one baseline corresponding to all inputs, hence the subsetting[0] here
         num_samples=approximation_steps,
         use_expectation=False,
     )
@@ -39,7 +42,7 @@ def path_explain_wrapper(
         model(inputs) - model(torch.full_like(inputs, baseline_fill)) - exp_ih.sum()
     )
 
-    exp_ih_reshaped = exp_ih.reshape(1, seqlen, 4, seqlen, 4)
+    exp_ih_reshaped = exp_ih.reshape(batch_size, seqlen, 4, seqlen, 4)
     exp_ih_reshaped.shape
 
     return exp_ih_reshaped, exp_ih_delta
